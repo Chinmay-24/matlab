481112e0f0664860997a192c6db77ebb7ec1deb8
Chinmay-24 <naiks.chinmay@gmail.com>
2025-11-21 12:31:13 +0530

    Create diff1.m

---
+++ digitalsignalprocessing/diff1.m
@@ -0,0 +1,31 @@
+x=[1,zeros(1,9)] %To simulate unit impulse
+y(1)=x(1) %This is actually y(0)
+y(2)=1.1*y(1)+x(2) %This is actually y(1)
+for n=3:10
+    y(n)=1.1*y(n-1)+x(n)
+end
+
+%To obtain impulse response using inbuilt command
+a=[1,-1.1] %Coefficients of y(n) & y(n-1)
+b=[1] %Coefficients of x(n)
+x=[1,zeros(1,9)] %To simulate unit impulse
+y1=filter(b,a,x) %To find the output due to unit impulse
+
+%To plot the impulse response
+stem(y)
+grid on
+xlabel('Discrete Time')
+ylabel('Amplitude')
+title('Impulse Response')
+
+%To obtain pole-zero plot
+a=[1,-1.1] %coefficients of y(n) and y(n-1)
+b=[1] %coefficient of x(n)
+figure
+zplane(b,a)
+title('pole-zero plot')
+
+%To plot magnitude response and phase response
+figure
+freqz(b,a)
+title('Magnitude Response and Phase Response')
